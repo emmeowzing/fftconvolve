5e74dc523e5a6566ff8c18d1aa5304a72cf8f9ca
bjd2385 <bjd2385@aperiodicity.com>
2016-07-25 01:34:29 -0400

    An example of using fftconv

--- test.py
+++ test.py
@@ -11,19 +11,19 @@ import matplotlib.pyplot as plt
 
 
 def main():
-	## Initialize a kernel
-	kern = _kernel.Kernel()
-	kern = kern.Kg2(13, 13, sigma=3.5, muX=0.0, muY=0.0)
-	kern /= np.sum(kern)	# normalize volume
+    ## Initialize a kernel
+    kern = _kernel.Kernel()
+    kern = kern.Kg2(13, 13, sigma=3.5, muX=0.0, muY=0.0)
+    kern /= np.sum(kern)    # normalize volume
 
-	## open an image
-	image = np.rot90(np.rot90(np.rot90(imread('spider.jpg').T[0])))
+    ## open an image
+    image = np.rot90(np.rot90(np.rot90(imread('spider.jpg').T[0])))
 
-	## Convolve using some method
-	conv = convolve(image, kern)
-	plt.imshow(conv.OAconv2(), interpolation='none', cmap='gray')
-	plt.show()
+    ## Convolve using some method
+    conv = convolve(image, kern)
+    plt.imshow(conv.OAconv2(), interpolation='none', cmap='gray')
+    plt.show()
 
 
 if __name__ == '__main__':
-	main()
+    main()
